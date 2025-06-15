d50a522fa2dcb038a4756fcb99e282cc5ec83df4
hidassmeryam@gmail.com <hidassmeryam@gmail.com>
2025-06-15 20:37:20 +0200

    feat: add contact form & animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,16 +5,26 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-extend: {
-  keyframes: {
-    scroll: {
-      '0%': { transform: 'translateX(0%)' },
-      '100%': { transform: 'translateX(-50%)' } // car on duplique les cartes
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' }
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(40px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' }
+        }
+      },
+      animation: {
+        scroll: 'scroll 20s linear infinite',
+        fadeInUp: 'fadeInUp 0.8s ease-out forwards',
+        fadeInRight: 'fadeInRight 0.8s ease-out forwards'
+      }
     }
-  },
-  animation: {
-    scroll: 'scroll 20s linear infinite'
   }
-}
-
-  }}
+};
