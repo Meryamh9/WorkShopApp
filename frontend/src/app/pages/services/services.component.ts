fc9419f252204f48ac36e909013bbb4912793af0
hidassmeryam@gmail.com <hidassmeryam@gmail.com>
2025-06-23 21:28:06 +0200

    update style

--- frontend/src/app/pages/services/services.component.ts
+++ frontend/src/app/pages/services/services.component.ts
@@ -1,21 +1,14 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-services',
-  imports: [MatCardModule],
+  imports: [CommonModule, MatCardModule],
   standalone: true,
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss',
 })
 export class ServicesComponent {
-  @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
 
-  scrollLeft() {
-    this.scrollContainer.nativeElement.scrollLeft -= 300;
-  }
-
-  scrollRight() {
-    this.scrollContainer.nativeElement.scrollLeft += 300;
-  }
 }
